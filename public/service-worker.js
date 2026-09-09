e646bc5b3d93bf8e404318295c35c4bbe5061d65
Anny Gabrielly <165099789+annygabb@users.noreply.github.com>
2026-09-09 08:32:53 -0300

    chore: refresh V10 iPhone app shell

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "uaiconta-shell-v5";
+const CACHE = "uaiconta-shell-v6";
 const SHELL = ["/", "/dashboard", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png", "/brand/uai-mark.svg"];
 
 self.addEventListener("install", (event) => {
